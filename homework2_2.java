e9b71810b2802c66a8049c6037b9806db97d3df3
Mackay <70379826+MM952263@users.noreply.github.com>
2020-09-27 17:21:47 -0400

    Add files via upload

---
+++ homework2_2.java
@@ -0,0 +1,14 @@
+public class homework2_2
+{
+         public static void main(String[] args)
+        {
+            
+            
+            double X = 8.5;
+            double Y = 15.3;
+            double Z = 4.8;
+            result = X + Y + Z * 1.06;
+            
+                System.out.println("Your total with tax is 30.3 dollars");
+          }
+}
